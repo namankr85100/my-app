e698b45d6caa16c32e7b830ed6be6b02dcc0e043
Naman Kumar <namankr91@gmail.com>
2024-01-12 17:05:20 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    return (
+      setCount(count+1)
+    )
+  }
+
+  
+  return (
+      <button onClick={handleClick}>
+          Clicked {count}
+      </button>
+  )
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+const listItems = products.map(product => 
+  <li
+    key = {product.id}
+    style={{
+      color: products.isFruit ? 'magenta': 'darkgreen'
+    }}
+  >
+    {product.title}
+  </li>  
+)
+
+
+function SharedStateButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+function ShareSameStateInTwoButton () {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count+1) 
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <SharedStateButton count={count} onClick={handleClick} />
+      <SharedStateButton count={count} onClick={handleClick} />
+    </div>
+  )
+
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      <MyButton />
+      <br/>
+      <br/>
+      <img
+       className='avatar'
+       src={user.imageUrl}
+       alt={'Photo of ' + user.name}
+       style={{
+         width: user.imageSize,
+         height: user.imageSize
+       }}
+      />
+     <br/>
+     <br/>
+      {listItems}
+       
+      <br/>
+      <ShareSameStateInTwoButton/>
     </div>
   );
 }
